ac8ceef78c2857c06969cf8f58817b56a8b2635c
vodchella <twister.kz@gmail.com>
2020-02-04 11:41:50 +0600

    Back button for subroutes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavigationActions } from 'react-navigation'
 import { View } from 'react-native'
 import { Button, Text } from 'native-base'
 import IconForButton from './IconForButton'
@@ -9,15 +10,15 @@ const HeaderComponent = ({ props }) => {
     const { routes, index } = navigation.state
     const route = routes[index]
     const routeName = route.routes[route.index].routeName
+    var action = route.index === 0 ? navigation.toggleDrawer : () => { navigation.dispatch(NavigationActions.back()) }
+    var icon = route.index === 0 ? 'menu' : 'arrow-back'
     return (
         <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
             <Button
                 rounded
-                onPress={() => {
-                    navigation.toggleDrawer()
-                }}
+                onPress={action}
             >
-                <IconForButton name='menu' />
+                <IconForButton name={icon} />
             </Button>
             <Text style={{ fontSize: 22, paddingLeft: 10 }}>
                 {ROUTE_PROPS[routeName].title}
